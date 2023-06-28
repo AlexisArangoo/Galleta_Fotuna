e53579d3fe3fc76afd2324bdd985e4f24c5e8bfc
Alexis Arango <134032888+AlexisArangoo@users.noreply.github.com>
2023-06-28 01:03:00 -0500

    sonido y animacion nueva

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,41 @@
 import './App.css'
 import Papelito from './components/Papelito'
 import frase from './data/frase.json'
-import { useState } from 'react'
 import Autor from './components/Autor'
+import { useState, useEffect } from 'react'
+import useSound from 'use-sound'
+import sonido from './assets/sonidos/sg.mp3'
 
 function App() {
 
+  const [playSonido] = useSound(sonido)
+
+  const [animacion, setAnimacion] = useState(false);
+
   const [index, setIndex] = useState(0)
 
-  const sonido = document.querySelector('#sonido')
-
-  const fondos = ['/fondos/f1.png', '/fondos/f2.png', '/fondos/f3.png', '/fondos/f4.png', '/fondos/f5.png', '/fondos/f6.png', '/fondos/f7.png', '/fondos/f8.png', '/fondos/f9.png', '/fondos/f10.png', '/fondos/f11.png', '/fondos/f12.png', '/fondos/f13.png', '/fondos/f14.png', '/fondos/f15.png',]
+  const fondos = ['/fondos/fondo-negro.png','/fondos/f1.png', '/fondos/f2.png', '/fondos/f3.png', '/fondos/f4.png', '/fondos/f5.png', '/fondos/f6.png', '/fondos/f7.png', '/fondos/f8.png', '/fondos/f9.png', '/fondos/f10.png', '/fondos/f11.png', '/fondos/f12.png', '/fondos/f13.png', '/fondos/f14.png', '/fondos/f15.png']
 
   const suerte = () => {
-    setIndex(Math.floor(Math.random() * ((frase.length - 1) - 0 + 1)) + 0)
-     
-    sonido.innerHTML = '<audio src="./public/sonidos/sg.mp3" autoPlay></audio>'
-  }
+    setIndex(Math.floor(Math.random() * ((frase.length - 1) - 1 + 1)) + 1)
+    playSonido()
+
+    setAnimacion(true)
+
+    setTimeout(() => {
+      setAnimacion(false);
+    }, 1000);
+  } 
   
   document.body.style = `background: url(${fondos[index]}) no-repeat center center fixed;
   background-size: cover;
   text-align: center;`
-
+  
   return (
     <> 
       <Papelito
       data={ frase[index] }
+      animacion={ animacion }
       />
 
       <button className='btn1' onClick={suerte}>PROBAR MI SUERTE</button>
@@ -33,7 +43,6 @@ function App() {
       <Autor
       data={ frase[index] }
       />
-      <div id='sonido'></div>
     </>
   )
 }
--- src/components/Papelito.jsx
+++ src/components/Papelito.jsx
@@ -1,8 +1,11 @@
-const Papelito = ({data}) => {
+import { useState, useEffect } from "react"
+
+const Papelito = ({data, animacion}) => {
+
     return(
         <div>
             <h1 className="titulo">GALLETAS DE LA FORTUNA</h1>
-            <article className="papel">
+            <article className={`papel ${animacion ? 'anim' : ''}`}>
                 <div className="par">{data.phrase}</div>
             </article>
         </div>
